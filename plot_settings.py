71c06230727a12b38923434e7c8040a964969121
Vanessa Graber <graber@ice.csic.es>
2022-04-20 16:24:12 +0200

    Fixing typos and reformatting code using black.

--- plot_settings.py
+++ plot_settings.py
@@ -1,32 +1,32 @@
 from matplotlib import rcParams
 
-rcParams['mathtext.fontset'] = 'stix'
-rcParams['font.family'] = 'Liberation serif'
-rcParams['font.size'] = '30'
+rcParams["mathtext.fontset"] = "stix"
+rcParams["font.family"] = "Liberation serif"
+rcParams["font.size"] = "30"
 # rcParams['font.weight']='bold'
-rcParams['figure.figsize'] = '8.0, 7.0'
-rcParams['figure.autolayout'] = True
+rcParams["figure.figsize"] = "8.0, 7.0"
+rcParams["figure.autolayout"] = True
 
-rcParams['axes.linewidth'] = '1.7'
-rcParams['axes.labelpad'] = '15.0'
-rcParams['axes.titlepad'] = '15.0'
+rcParams["axes.linewidth"] = "1.7"
+rcParams["axes.labelpad"] = "15.0"
+rcParams["axes.titlepad"] = "15.0"
 
-rcParams['xtick.direction'] = 'in'
-rcParams['xtick.top'] = True
-rcParams['xtick.major.pad'] = '10.0'
-rcParams['xtick.minor.pad'] = '10.0'
-rcParams['xtick.major.size'] = '10.0'
-rcParams['xtick.major.width'] = '1.7'
-rcParams['xtick.minor.size'] = '5.0'
-rcParams['xtick.minor.width'] = '1.7'
-rcParams['xtick.labelsize'] = '30'
+rcParams["xtick.direction"] = "in"
+rcParams["xtick.top"] = True
+rcParams["xtick.major.pad"] = "10.0"
+rcParams["xtick.minor.pad"] = "10.0"
+rcParams["xtick.major.size"] = "10.0"
+rcParams["xtick.major.width"] = "1.7"
+rcParams["xtick.minor.size"] = "5.0"
+rcParams["xtick.minor.width"] = "1.7"
+rcParams["xtick.labelsize"] = "30"
 
-rcParams['ytick.direction'] = 'in'
-rcParams['ytick.right'] = True
-rcParams['ytick.major.pad'] = '10.0'
-rcParams['ytick.minor.pad'] = '10.0'
-rcParams['ytick.major.size'] = '10.0'
-rcParams['ytick.major.width'] = '1.7'
-rcParams['ytick.minor.size'] = '5.0'
-rcParams['ytick.minor.width'] = '1.7'
-rcParams['ytick.labelsize'] = '30'
+rcParams["ytick.direction"] = "in"
+rcParams["ytick.right"] = True
+rcParams["ytick.major.pad"] = "10.0"
+rcParams["ytick.minor.pad"] = "10.0"
+rcParams["ytick.major.size"] = "10.0"
+rcParams["ytick.major.width"] = "1.7"
+rcParams["ytick.minor.size"] = "5.0"
+rcParams["ytick.minor.width"] = "1.7"
+rcParams["ytick.labelsize"] = "30"
